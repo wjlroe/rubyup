1f38d167ef9cfcf56cc98f8953176cb697f2a710
William Roe <git@wjlr.org.uk>
2017-06-24 17:02:54 +0100

    WIP: Try to find already installed ruby
    Fighting the borrow checker

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,38 @@ extern crate binary_shim;
 extern crate clap;
 
 use clap::{App, SubCommand};
-use std::fs::File;
+use std::fs::{File, read_dir};
 use std::io::{Error, Read};
 use std::path::Path;
 
 type RunResult = Result<(), Error>;
+
+fn find_a_ruby() -> Result<(), Box<Error>> {
+    // TODO: _Actually_ find a Ruby, nice to have!
+    if cfg!(target_os = "windows") {
+        for direntry in read_dir("C:")? {
+            let entryref = &direntry;
+            if entryref
+                .and_then(|entry| entry.file_type())
+                .map(|file| file.is_dir())
+                .unwrap_or(false)
+            {
+                println!("dir: {:?}", direntry);
+            }
+        }
+        // let dirs = read_dir("C:")?
+        //     .filter(|entry| {
+        //                 entry
+        //                     .and_then(|entry| entry.file_type())
+        //                     .map(|file| file.is_dir())
+        //                     .unwrap_or(false)
+        //             });
+        // for dir in dirs {
+        //     println!("dir: {:?}", dir);
+        // }
+    }
+    Ok(())
+}
 
 fn run_a_ruby() -> RunResult {
     // find a .ruby-version file
@@ -40,6 +67,7 @@ fn main() {
                     .about("show the available and selected rubies"))
         .get_matches();
 
+    find_a_ruby();
     if let Some(_) = matches.subcommand_matches("show") {
         let _ = show_rubies();
     } else {
